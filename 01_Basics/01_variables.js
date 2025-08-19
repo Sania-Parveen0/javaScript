9d333bcbf610eb6e755c4a69301e09843af1134f
Sania Parveen <saniaparveen861@gmail.com>
2025-08-19 04:46:39 +0000

    done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "saniaparveen@gmail.com"
+var accountPassword="12345"
+accountCity="kolkata"
+
+accountId = 215
+accountEmail = "saniaparveen124@gmail.com"
+accountPassword="sania_542"
+accountCity = "durgapur"
+
+console.log(accountId);
