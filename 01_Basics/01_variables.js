5113ba15ced9042c4fd1e67dbd64c8e6c12820c5
Sania Parveen <saniaparveen861@gmail.com>
2025-08-19 16:37:25 +0000

    datatypes

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -3,11 +3,22 @@ let accountEmail = "saniaparveen@gmail.com"
 var accountPassword="12345"
 accountCity="kolkata"
 
-accountId = 215
+let accountState;
+
+// accountId = 215  //not allowed
 accountEmail = "saniaparveen124@gmail.com"
 accountPassword="sania_542"
 accountCity = "durgapur"
 
 console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId , accountPassword, accountEmail,accountCity, accountState])
+
+
 
 
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treate all js code as newer version
+
+// alert("hello") //we are using nodejs not browser
+
+console.log(3+3);
+
+console.log("Sania");
+
+
+let name = "sayan"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 th the power 53
+//bigInt
+//String => "string"
+// boolean =>true / false
+//null => standalone value
+//undefined => value has not assigned
+//symbol =>unique
+
+
+//object
+console.log(typeof null); // null is an object // type of null is object
+
