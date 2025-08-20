7023cc394ecc90b16cde8134079d3672c186101e
Sania Parveen <saniaparveen861@gmail.com>
2025-08-20 19:33:22 +0000

    datatype summary

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,32 @@
+/* primitive
+    7 types: string , number , boolean , null , undefind , symbol , Bigint ,  
+
+    call by value
+*/
+
+const id = Symbol('123')
+
+const bigNumber = 126845588457555n
+
+/* reference(non primitive)
+
+    Array , objects , functions
+
+*/
+const heros = ["shaktiman" , "naagraj" , "doga"]
+
+let myObj= {
+    name: "sania",
+    age: 20,
+}
+
+ const  muFunction = function () {
+    console.log("hello world");
+    
+}
+
+console.log(typeof bigNumber)
+
+/* 
+
+*/
