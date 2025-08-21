8b5a06fe3ca5221383511ceb5903d2917ea51661
Sania Parveen <saniaparveen861@gmail.com>
2025-08-21 10:11:52 +0000

    nums

---
+++ 01_Basics/06_nums_math.js
@@ -0,0 +1,19 @@
+const score = 400
+
+const balance = new Number(100)
+
+console.log(balance);
+console.log(score);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber = 12.225471
+
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+// **********************************maths*******************************************
