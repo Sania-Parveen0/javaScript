e1384b3c71605dfe4b79bc27efc2769e92d70a02
Sania Parveen <saniaparveen861@gmail.com>
2025-08-21 07:15:33 +0000

    string

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,14 @@
+const name = "sayan"
+const repoCount = 50
+
+//  console.log(name + repoCount + " value"); 
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('saniaParveen') // good syntax
+
+const newString = gameName.substring(2,3)
+console.log(newString);
+
+const anotherString = gameName.slice(-5,2)
+console.log(anotherString);
