b5047371d0dab960b3ad8cce5af66890905d78ea
Sania Parveen <saniaparveen861@gmail.com>
2025-08-21 15:43:25 +0000

    math.num

--- 01_Basics/06_nums_math.js
+++ 01_Basics/06_nums_math.js
@@ -2,18 +2,41 @@ const score = 400
 
 const balance = new Number(100)
 
-console.log(balance);
-console.log(score);
+// console.log(balance);
+// console.log(score);
 
-console.log(balance.toString());
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
 
 const otherNumber = 12.225471
 
-console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(3));
 
 const hundreds = 1000000000
-console.log(hundreds.toLocaleString('en-IN'));
+// console.log(hundreds.toLocaleString('en-IN')); //for indian currency we use ('en-IN)
 
 // **********************************maths*******************************************
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.8));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(4, 5 , 8 , 6 ,1));
+// console.log(Math.max(4, 5 , 8 , 6 ,1));
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10)+1);
+
+// gives number between 1 to 6
+
+
+ //console.log(Math.floor(Math.random()*6)+1)
+
+ // gives number between 10 to 20
+
+const min = 10
+const max = 20
+
+
+console.log(Math.floor(Math.random()*(max -min +1))+min)
