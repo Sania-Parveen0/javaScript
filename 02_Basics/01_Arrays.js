10e94f8b2442757d87464e3cb0ca468c87303548
Sania Parveen <saniaparveen861@gmail.com>
2025-08-22 06:55:53 +0000

    Array part1

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,46 @@
+//Arrays
+
+const myArr = [ 1 ,2 ,3,4,5,6]
+
+const names = ["Ariyan","Sania", "sayan"  ]
+//console.log(names[0]);
+
+
+
+const myArr2 = new Array(1,5,6,8,10,15,20)
+
+//console.log(myArr[0]);
+
+
+//Array Methods
+
+// myArr.push(7)
+// myArr.push(8)
+// myArr.pop()
+
+
+// myArr.unshift(0)
+// myArr.shift()
+
+
+// console.log(myArr.includes(4))
+// console.log(myArr.indexOf(50))
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr)
+
+
+console.log("A=",myArr)
+
+
+// const mySlArr = myArr.slice(0,3) 
+// console.log("slice",mySlArr)
+// console.log("B=",myArr)
+
+
+const mySlArr2 = myArr.splice(0,6) 
+console.log("splice",mySlArr2)
+console.log("c=",myArr)
+
