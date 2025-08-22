14cf79e4b2fd23556fef55948f1c16428138006c
Sania Parveen <saniaparveen861@gmail.com>
2025-08-22 18:48:01 +0000

    Arrays done

---
+++ 02_Basics/02_Arrays.js
@@ -0,0 +1,45 @@
+let marvel_heros = ["Iron Man","Spider Man","Thor","Hulk"]
+
+let DC_heros = ["Super Man","Bat Man","Flash"]
+
+//marvel_heros.push(DC_heros)
+
+// console.log(marvel_heros);
+
+// console.log(marvel_heros[4][0]);
+
+// let allHeros = marvel_heros.concat(DC_heros); //concat method
+// console.log(allHeros);
+
+
+// let allNewHeros = [...marvel_heros,...DC_heros] //spread
+// console.log(allNewHeros);
+
+
+let hardCase = [1,2,3,[4,5,6],7[6,7,[4,5]]]
+
+let solveCase = hardCase.flat(Infinity)
+
+// console.log(solveCase);
+
+
+
+
+// console.log(Array.isArray("Sania"))
+
+//  let myObj = {
+//     name:"sania",
+//     title:"parveen"
+// };
+
+//  let objArr=Array.from(Object.entries(myObj))
+
+//  console.log(objArr);
+
+
+let score1 = 12
+let score2 = 24
+
+console.log(Array.of(score1,score2));
+
+ 
