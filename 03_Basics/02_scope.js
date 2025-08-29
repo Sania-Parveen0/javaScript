9d59ba899adefc769ec846ea179bc45c1464ed6b
Sania Parveen <saniaparveen861@gmail.com>
2025-08-29 06:30:49 +0000

    Scope done

---
+++ 03_Basics/02_scope.js
@@ -0,0 +1,46 @@
+
+ function one() {
+     const username = "sania"
+
+     function two() {
+         const price = 999
+    console.log(username);
+        
+   }
+    // console.log(price);
+    
+    //two()
+ }
+
+ // one()
+
+
+if(true){
+    const username = "sayan"
+    if(username==="sayan"){
+        const web = "sania"
+        //console.log(username + web );
+    
+    }
+   // console.log(web);
+    
+}
+// console.log(username);
+
+
+// *************************************interesting*******************************************
+
+// console.log(addOne(5));
+
+function addOne(num) {
+    return num +1
+}
+
+
+
+
+const addTwo = function (num) {
+    return num+2
+}
+
+// console.log(addTwo(5));
