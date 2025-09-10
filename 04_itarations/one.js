d3d1f160a6f0906246a0ba13c47f7bac438674f1
Sania Parveen <saniaparveen861@gmail.com>
2025-09-10 15:56:00 +0000

    loop done

---
+++ 04_itarations/one.js
@@ -0,0 +1,23 @@
+
+// let i = 1
+// while (i<=10) {
+//     console.log(`value of i is ${i}`);
+//     i=i+2
+// }
+
+let myArr=["superman","batman","flash"]
+let arr = 0
+while(arr<myArr.length){
+    // console.log(myArr[arr]);
+    arr = arr+1
+    
+}
+
+
+let marks=15
+do {
+    console.log(`marks is ${marks}`);
+    marks++
+    
+} while (marks<=10);
+
