24eefd4664066171801a13a64fd9b2a98b8467b8
Sania Parveen <saniaparveen861@gmail.com>
2025-09-11 14:03:37 +0000

    for each done

---
+++ 04_itarations/four.js
@@ -0,0 +1,46 @@
+const pLang = ["js","py","java","ruby","c","c++","R"]
+
+pLang.forEach((nm) => {
+    // console.log(nm)
+});
+
+pLang.forEach(function (val){
+    // console.log(val);
+    
+})
+
+const Prgmng =[
+    {
+        name:"sania",
+        age:20,
+        gender: "female"
+    },
+    {
+        name:"sania",
+        age:20,
+        gender: "female"  
+    },
+    {
+        name:"sania",
+        age:20,
+        gender: "female"
+    },
+    {
+        name:"sania",
+        age:20,
+        gender: "female"
+    },
+    {
+        name:"sania",
+        age:20,
+        gender: "female"
+    },
+]
+Prgmng.forEach((item)=>{
+    console.log(item.name);
+    
+})
+
+// array.forEach(element => {
+    
+// });
